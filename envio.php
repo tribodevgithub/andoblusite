15a39d21398dbcc530bbf82a6885a76da358de76
Dário Silva <dariooosilvaaa@gmail.com>
2022-08-25 16:50:10 -0300

    Update Email

--- envio.php
+++ envio.php
@@ -15,17 +15,17 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
 
 
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'dariooosilvaaa@gmail.com';                     //SMTP username
-    $mail->Password   = 'dwvoliwjdgsvxdkx';                               //SMTP password
+    $mail->Username   = 'no-reply@andoblu.com.br';                     //SMTP username
+    $mail->Password   = '#srCwxZaF$3x32s';                               //SMTP password
     $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('dariooosilvaaa@gmail.com', 'Mailer');
-    $mail->addAddress('dariooosilvaaa@gmail.com', 'Dário Silva');     //Add a recipient
-    $mail->addReplyTo('dariooosilvaaa@gmail.com', 'Information');
+    $mail->setFrom('no-reply@andoblu.com.br', 'Mailer');
+    $mail->addAddress('no-reply@andoblu.com.br', 'Andoblu');     //Add a recipient
+    $mail->addReplyTo('no-reply@andoblu.com.br', 'Information');
     $mail->isHTML(true);                                  
     $mail->Subject = 'Mensagem enviada do Site - Andoblu';
 
